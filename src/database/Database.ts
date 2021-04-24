72be2ca0f7720890d9054f294ba81d5e56158b51
gediminasnn <neniusgediminas@gmail.com>
2021-04-24 17:03:26 +0300

    improved database to be like orm

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,29 +1,36 @@
 import fs from 'fs';
 import path from 'path';
-import IDatabase from './IDatabase';
+import fsExists from 'fs.promises.exists';
 import { IUser, IPhoto, ITag, DatabaseTableNames, DatabaseTemplate } from '../types';
 
-export default class Database implements IDatabase {
-  protected databasePath = path.resolve(`${__dirname}/../../database.json`);
-  public async databaseInit() {
-    const doesDatabaseExist = fs.existsSync(this.databasePath);
+export default class Database {
+  public static databasePath = path.resolve(`${__dirname}/../../database.json`);
+
+  public static async databaseInit() {
+    const doesDatabaseExist = await fsExists(this.databasePath);
+
     if (!doesDatabaseExist) {
       const database: DatabaseTemplate = { users: [], photos: [], tags: [] };
-      fs.writeFileSync(this.databasePath, JSON.stringify(database));
+
+      await fs.promises.writeFile(this.databasePath, JSON.stringify(database));
+
       console.log('Database was created');
     }
   }
-  public readDatabase() {
+
+  public static async readDatabase() {
     if (!fs.existsSync(this.databasePath)) {
       throw new Error('Database does not exists');
     }
 
-    const data = fs.readFileSync(this.databasePath, 'utf8');
+    const data = await fs.promises.readFile(this.databasePath, 'utf8');
+
     return JSON.parse(data);
   }
-  public writeToDatabase(data: IUser[] | IPhoto[] | ITag[], databaseTable: DatabaseTableNames) {
-    const dbData = this.readDatabase();
-    fs.writeFileSync(
+
+  public static async writeToDatabase(data: IUser[] | IPhoto[] | ITag[], databaseTable: DatabaseTableNames) {
+    const dbData = await this.readDatabase();
+    await fs.promises.writeFile(
       this.databasePath,
       JSON.stringify({
         ...dbData,
--- src/database/IDatabase.ts
+++
@@ -1,7 +0,0 @@
-import { IUser, IPhoto, ITag, DatabaseTableNames, DatabaseTemplate } from '../types';
-
-export default interface IDatabase {
-  databaseInit(): void;
-  readDatabase(): DatabaseTemplate;
-  writeToDatabase(data: IUser[] | IPhoto[] | ITag[], databaseTable: DatabaseTableNames): void;
-}
