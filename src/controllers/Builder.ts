6c68db007528de60377c56ae08be7f0b0dc42eb7
gediminasnn <neniusgediminas@gmail.com>
2021-04-24 17:04:29 +0300

    improved builder to be compatible with new async functions

--- src/controllers/Builder.ts
+++ src/controllers/Builder.ts
@@ -1,30 +1,21 @@
 import IBuilder from './IBuilder';
-import IDatabase from '../database/IDatabase';
-import { DatabaseTableNames, IUser, IPhoto } from '../types';
+import { IUser, IPhoto } from '../types';
 import { User, Photo, Tag } from '../models';
 
 export default class Builder implements IBuilder {
   /* eslint-disable no-useless-constructor */
-  constructor(
-    private database: IDatabase,
-    private usersCount: number = 1,
-    private photosCount: number = 10,
-    private tagsCount: number = 10
-  ) {}
+  constructor(private usersCount: number = 5, private photosCount: number = 5, private tagsCount: number = 5) {}
 
-  public appendDataToDatabase(): void {
+  public async appendNewDataToDatabase() {
     // Generating and writing users to database.json
-    const users = User.createBulk(this.usersCount);
-    this.database.writeToDatabase(users, DatabaseTableNames.USERS);
+    const users = await User.createBulk(this.usersCount);
 
     // Generating and writing photos to database.json
     const usersIds = users.map((user: IUser) => user.id);
-    const photos = Photo.createBulk(this.photosCount, usersIds);
-    this.database.writeToDatabase(photos, DatabaseTableNames.PHOTOS);
+    const photos = await Photo.createBulk(this.photosCount, usersIds);
 
     // Generating and writing tags to database.json
     const photosIds = photos.map((photo: IPhoto) => photo.id);
-    const tags = Tag.createBulk(this.tagsCount, photosIds);
-    this.database.writeToDatabase(tags, DatabaseTableNames.TAGS);
+    await Tag.createBulk(this.tagsCount, photosIds);
   }
 }
