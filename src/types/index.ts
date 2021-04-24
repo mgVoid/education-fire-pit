889945e67ca03b09583f61cfce4bb6df2b5e1ecf
gediminasnn <neniusgediminas@gmail.com>
2021-04-24 14:36:19 +0300

    naming convention explanation added

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,7 @@ export type { default as IUser } from './IUser';
 export type { default as IPhoto } from './IPhoto';
 export type { default as ITag } from './ITag';
 export type { default as IHasId } from './IHasId';
-export type { default as IHasTimestamp } from './IHasTimestamp';
-export { default as UserStatuses } from './UserStatuses';
+export type { default as IHasTimestamp } from './IHasTimestamp'; // interfaces naming starts with I
+export { default as UserStatuses } from './UserStatuses'; // enums names are plural
 export { default as DatabaseTableNames } from './DatabaseTableNames';
-export type { default as DatabaseTemplate } from './DatabaseTemplate';
+export type { default as DatabaseTemplate } from './DatabaseTemplate'; // types names are singular
