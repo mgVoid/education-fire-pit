9f0e5035953e384e79e54e86507ffac03e140ff9
gediminasnn <neniusgediminas@gmail.com>
2021-04-24 17:04:46 +0300

    improved analytics controller to be compatible with async func

--- src/controllers/Analytics.ts
+++ src/controllers/Analytics.ts
@@ -1,14 +1,12 @@
-import IDatabase from '../database/IDatabase';
 import { User } from '../models';
 
 export default class Analytics {
   // eslint-disable-next-line no-useless-constructor
-  constructor(private database: IDatabase) {}
 
-  displayRandomUserData(): void {
-    const users = User.findAll(this.database);
+  static async displayRandomUserData(): Promise<void> {
+    const users = await User.findAll();
     const randomUser = users[Math.floor(Math.random() * users.length)];
-    const fullUserData = User.getUserWithRelations(randomUser.id, this.database);
+    const fullUserData = await User.getUserWithRelations(randomUser.id);
 
     console.dir(fullUserData, { depth: null });
   }
